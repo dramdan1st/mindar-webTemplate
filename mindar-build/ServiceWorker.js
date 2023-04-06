cb579e80cf638f2880553905cb67871443547c86
dramdan1st <34118613+dramdan1st@users.noreply.github.com>
2023-04-06 11:30:24 +0700

    sample
    new project

---
+++ mindar-build/ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "WolfpupLabs-MindAR-0.1";
+const contentToCache = [
+    "Build/mindar-build.loader.js",
+    "Build/mindar-build.framework.js.unityweb",
+    "Build/mindar-build.data.unityweb",
+    "Build/mindar-build.wasm.unityweb",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
